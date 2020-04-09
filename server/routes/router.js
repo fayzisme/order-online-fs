42e08da04263b4bc09a7de5dee8cbf88010a475d
fayzisme <fayzisme@gmail.com>
2020-04-09 15:28:45 +0700

    Mengatur Relasi Database

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
-const jodoh = require('../controller/jodoh.controller.js')
+const jodoh = require('../controller/controller.js')
 
 router.get('/', function(request, response) {
     response.send('Webserver fayzisme')
 })
-router.get('/cari-jodoh', jodoh.getJodoh)
-router.post('/cari-jodoh', jodoh.setJodoh)
-router.put('/cari-jodoh', jodoh.updateJodoh)
-router.delete('/cari-jodoh', jodoh.deleteJodoh)
+router.get('/', jodoh.getJodoh)
+router.post('/', jodoh.setJodoh)
+router.put('/', jodoh.updateJodoh)
+router.delete('/', jodoh.deleteJodoh)
 router.get('/about', (req, res) => {
     res.send('ini rute /about ' + req.query.nama + ' dan umurnya ' + req.query.umur)
 })
